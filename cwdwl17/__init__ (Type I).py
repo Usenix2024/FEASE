e8daaa8023b65781ff0ad9857eb0d18fa88b641a
Usenix2024 <134785502+Usenix2024@users.noreply.github.com>
2023-05-27 14:57:51 +0200

    Add files via upload

--- cwdwl17/__init__ (Type I).py
+++ cwdwl17/__init__ (Type I).py
@@ -12,7 +12,7 @@ Hui Cui, Zhiguo Wan, Robert H.Deng, Guilin Wang, Yingjiu Li
 from charm.toolbox.pairinggroup import PairingGroup,ZR,G1,G2,GT,pair
 from charm.toolbox.ABEnc import ABEnc, Input, Output
 from msp import MSP
-import copy
+import copy, time
 from secretutil import SecretUtil
 from policytree import PolicyParser
 
@@ -28,32 +28,24 @@ class KPabe_search(ABEnc):
 
     def setup(self):
         g, g_hat = self.group.random(G1), self.group.random(G2)
-        x1, x2, x3 = self.group.random(), self.group.random(), self.group.random()
-        u = g ** x1
-        u_hat = g_hat ** x1
-        h = g ** x2
-        h_hat = g_hat ** x2
-        w = g ** x3
-        w_hat = g_hat ** x3        
-        #u, h, w = self.group.random(G1), self.group.random(G1), self.group.random(G1) 
-        #u_hat, h_hat, w_hat = self.group.random(G2), self.group.random(G2), self.group.random(G2) 
+        u, h, w = self.group.random(G1), self.group.random(G1), self.group.random(G1) 
         alpha, d1, d2, d3, d4 = self.group.random(ZR), self.group.random(ZR), self.group.random(ZR), self.group.random(ZR), self.group.random(ZR)  
 
-        g1 = g ** d1
-        g2 = g ** d2
-        g3 = g ** d3
-        g4 = g ** d4
+        g1 = g_hat ** d1
+        g2 = g_hat ** d2
+        g3 = g_hat ** d3
+        g4 = g_hat ** d4
 
-        e_gg_alpha = pair(g, g_hat) ** alpha
+        e_gg_alpha = pair(g, g_hat ** alpha)
         
-        pk = { 'g':g, 'g_hat':g_hat, 'u':u, 'h':h, 'w':w, 'g1':g1, 'g2':g2, 'g3':g3, 'g4':g4, 'e_gg_alpha':e_gg_alpha }
-        msk = {'alpha':alpha, 'u_hat': u_hat, 'h_hat': h_hat, 'w_hat': w_hat, 'd1':d1, 'd2':d2, 'd3':d3, 'd4':d4 }
+        pk = { 'g':g, 'u':u, 'h':h, 'w':w, 'g_hat':g_hat, 'g1':g1, 'g2':g2, 'g3':g3, 'g4':g4, 'e_gg_alpha':e_gg_alpha }
+        msk = {'alpha':alpha, 'd1':d1, 'd2':d2, 'd3':d3, 'd4':d4 }
               
         return (pk, msk)
 
     def skeygen(self, pk):                    
         gamma = self.group.random(ZR)
-        pk_s = pk['g'] ** gamma
+        pk_s = pk['g_hat'] ** gamma
         sk_s = gamma     
         
         return pk_s, sk_s
@@ -78,9 +70,8 @@ class KPabe_search(ABEnc):
             rand = self.group.random(ZR)
             v.append(rand)        
 
-        r, r_prime = self.group.random(ZR), self.group.random(ZR)
-        T = pk['g'] ** r
-        T_prime = pk['g_hat'] ** r_prime
+        r = self.group.random(ZR)
+        T = pk['g_hat'] ** r
         T1, T2, T3, T4, T5, T6 = {}, {}, {}, {}, {}, {}
         
         for kw, row in mono_span_prog.items():
@@ -90,22 +81,20 @@ class KPabe_search(ABEnc):
             len_row = len(row)
             Mivtop = sum(i[0] * i[1] for i in zip(row, v[:len_row]))          
             t1, t2 = self.group.random(ZR), self.group.random(ZR)
-            T1[k] = pk['g_hat'] ** Mivtop * mk['w_hat']**(mk['d1']*mk['d2']*t1+mk['d3']*mk['d4']*t2)
+            T1[k] = pk['g'] ** Mivtop * pk['w']**(mk['d1']*mk['d2']*t1+mk['d3']*mk['d4']*t2)
+            T2[k] = pk_s ** r * pk['g_hat']**(mk['d1']*mk['d2']*t1+mk['d3']*mk['d4']*t2) 
+            T3[k] = ((pk['u'] ** kwHash * pk['h'])**t1)**(-mk['d2'])
+            T4[k] = ((pk['u'] ** kwHash * pk['h'])**t1)**(-mk['d1'])
+            T5[k] = ((pk['u'] ** kwHash * pk['h'])**t2)**(-mk['d4'])
+            T6[k] = ((pk['u'] ** kwHash * pk['h'])**t2)**(-mk['d3'])
             
-            tem = self.group.hash(str(pair(pk_s, T_prime) ** r), ZR)
-            
-            T2[k] = pk['g_hat'] ** (mk['d1']*mk['d2']*t1+mk['d3']*mk['d4']*t2 + tem)
-            T3[k] = ((mk['u_hat'] ** kwHash * mk['h_hat'])**t1)**(-mk['d2'])
-            T4[k] = ((mk['u_hat'] ** kwHash * mk['h_hat'])**t1)**(-mk['d1'])
-            T5[k] = ((mk['u_hat'] ** kwHash * mk['h_hat'])**t2)**(-mk['d4'])
-            T6[k] = ((mk['u_hat'] ** kwHash * mk['h_hat'])**t2)**(-mk['d3'])
-            
-        return { 'T':T, 'T_prime':T_prime, 'T1':T1, 'T2':T2, 'T3':T3, 'T4':T4, 'T5':T5, 'T6':T6, 'policy':str(policy_stripped) }  
+        return { 'T':T, 'T1':T1, 'T2':T2, 'T3':T3, 'T4':T4, 'T5':T5, 'T6':T6, 
+            'policy':str(policy_stripped) }  
    
     def encrypt(self, pk, W):
         mu = self.group.random() 
         C = pk['e_gg_alpha']**mu
-        D = pk['g']**mu
+        D = pk['g_hat']**mu
         D_i, E1, E2, F1, F2 = {}, {}, {}, {}, {}
         for i in W:
             z = self.group.random()      # default: ZR
@@ -122,6 +111,8 @@ class KPabe_search(ABEnc):
 
     def search(self, pk, trap, ct, sk_s):
         #if debug: print("\nTrapdoor's policy: ", trap['policy'])
+        start = time.time()
+
         policy = util.createPolicy(trap['policy'])        
         pruned_list = util.prune(policy, ct['kws'], 1)     
         
@@ -133,22 +124,25 @@ class KPabe_search(ABEnc):
         rslt = 0
         num_keywords = 0
         num_tests = 0
-        tep = pk['g_hat'] ** self.group.hash(str(pair(trap['T'], trap['T_prime'])**sk_s), ZR)
         for attr_list in pruned_list:
             #if debug: print("\nKeywords for testing:", attr_list)
             A = 1 
             for i in attr_list:
                 j = i.getAttributeAndIndex(); k = i.getAttribute() 
-                B = pair(ct['D'], trap['T1'][k]) * pair(ct['D_i'][k], trap['T2'][k]/tep) * pair(ct['E1'][k], trap['T3'][k]) * pair(ct['E2'][k], trap['T4'][k]) * pair(ct['F1'][k], trap['T5'][k]) * pair(ct['F2'][k], trap['T6'][k]) #** z[j]
+                
+                B = ( pair(trap['T1'][k], ct['D']) * pair(ct['D_i'][k], trap['T2'][k]/trap['T']**sk_s) * 
+                       pair(trap['T3'][k], ct['E1'][k]) * pair(trap['T4'][k], ct['E2'][k]) * 
+                       pair(trap['T5'][k], ct['F1'][k]) * pair(trap['T6'][k], ct['F2'][k]) ) #** z[j]
                 A *= B
             num_tests += 1
             num_keywords += len(attr_list)
-           
-            if A == ct['C'] :
+            if ( A == ct['C'] ):
                 result = 1
-
                 break
             else:
                 result = 0                                              
                 continue      
+        end = time.time()
+        times = format((end - start) * 1000, '7.2f')   
+        print(times)               
         return pruned_list, result
